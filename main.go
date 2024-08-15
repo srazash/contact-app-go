51acc1d129be2c346fd0faffb6f3b51fcf996177
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-08-15 13:39:29 +0100

    Cleanup of main.go

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"contactapp/controllers/counter"
 	"contactapp/models/contact"
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,21 +12,6 @@ import (
 func main() {
 	contact.Load()
 	counter.Load()
-	ptrDB := contact.Ptr()
-	log.Printf("loaded %d contacts\n", len(*ptrDB))
-	log.Printf("loaded %d visitors\n", counter.Count)
-
-	// static := http.FileServer(http.Dir("static"))
-	// http.Handle("/static/", http.StripPrefix("/static/", static))
-
-	// http.HandleFunc("/", serve.Root)
-	// http.HandleFunc("/contacts", serve.Contacts)
-	// http.HandleFunc("/contacts/new", serve.ContactsNew)
-	// http.HandleFunc("/contacts/", serve.ContactsShowEdit)
-
-	// port := ":3000"
-	// log.Printf("http://localhost%s\n", port)
-	// log.Fatal(http.ListenAndServe(port, nil))
 
 	e := echo.New()
 
