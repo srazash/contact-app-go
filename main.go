25bdc7a7162fec00e1031f617fda840996e77558
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-08-17 10:48:02 +0100

    Template is now partially loading

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"contactapp/controllers/counter"
 	"contactapp/models/contact"
+	"fmt"
 	"html/template"
 	"io"
 	"net/http"
@@ -16,7 +17,11 @@ type Templates struct {
 }
 
 func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.template.ExecuteTemplate(w, name, data)
+	err := t.template.ExecuteTemplate(w, "layout", data)
+	if err != nil {
+		c.Logger().Errorf("template rendering error: %v", err)
+	}
+	return err
 }
 
 func main() {
@@ -25,28 +30,46 @@ func main() {
 
 	e := echo.New()
 
-	renderer := &Templates{
-		template: template.Must(template.ParseGlob("views/*.html")),
-	}
-	e.Renderer = renderer
-
 	e.Use(middleware.Logger())
 	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
 		Level: 5,
 	}))
 	e.Static("/static", "static")
 
+	templates, err := template.ParseGlob("views/*.html")
+	if err != nil {
+		fmt.Printf("Error parsing templates: %v\n", err)
+		return // or handle the error appropriately
+	}
+
+	renderer := &Templates{
+		template: templates,
+	}
+
+	fmt.Printf("Number of templates loaded: %d\n", len(templates.Templates()))
+
+	e.Renderer = renderer
+
 	e.GET("/", func(c echo.Context) error {
 		return c.Redirect(http.StatusFound, "/contacts")
 	})
 
 	e.GET("/contacts", func(c echo.Context) error {
-		c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTMLCharsetUTF8)
-		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+		data := map[string]interface{}{
 			"Term":     "",
 			"Contacts": *contact.Ptr(),
 			"Counter":  counter.Count,
-		})
+			"Debug":    "This is a debug message",
+		}
+		err := c.Render(http.StatusOK, "index.html", data)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Template error: "+err.Error())
+		}
+		return nil
+	})
+
+	e.GET("/debug", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "<h1>Debug Page</h1>")
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
