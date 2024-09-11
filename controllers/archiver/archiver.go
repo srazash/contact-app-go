476ce3c2687ce39127bb54e04395758aa931f9ff
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-09-11 22:00:38 +0100

    Added archiver API package

---
+++ controllers/archiver/archiver.go
@@ -0,0 +1,25 @@
+package archiver
+
+func status() string {
+	return ""
+}
+
+func progress() float64 {
+	return 0.0
+}
+
+func run() {
+
+}
+
+func reset() {
+
+}
+
+func archive_file() string {
+	return ""
+}
+
+func get() {
+
+}
